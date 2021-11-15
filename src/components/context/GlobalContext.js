b99989bbe99814fe5438085d30fa2cd11889d1dc
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-11-15 18:16:47 -0300

    fix bug when saving cart to local storage

--- src/components/context/GlobalContext.js
+++ src/components/context/GlobalContext.js
@@ -6,76 +6,76 @@ const GlobalContext = createContext({});
 export function GlobalProvider({ children }) {
 	const [userData, setUserData] = useState({});
 	const [cartProducts, setCartProducts] = useState([]);
-	const [ cartId, setCartId ] = useState(-1)
-	const [ update, setUpdate ] = useState(0)
+	const [cartId, setCartId] = useState(-1);
+	const [update, setUpdate] = useState(0);
 
 	//load locally storaged data, if a token is found, request the cart data from server
 	useEffect(() => {
-		console.log("RUNNING GLOBAL USE EFFECT LOAD CART")
+		console.log("RUNNING GLOBAL USE EFFECT LOAD CART");
 		const storagedData = getUserFromLocalStorage();
-		console.log("storagedData: ", storagedData)
-		if(userData.token) {
-			console.log("LOADING GLOBAL CONTEXT CART DATA FROM BACKEND")
-			console.log("sending this token: ", userData.token)
-			API.getCart(userData.token)
-				.then(res => {
-					console.log("CART RECEIVED")
-					console.log("RESPONSE: ", res)
-					setCartId(res.cartId)
-					setCartProducts(res.products)
-				})
+		console.log("storagedData: ", storagedData);
+		if (userData.token) {
+			console.log("LOADING GLOBAL CONTEXT CART DATA FROM BACKEND");
+			console.log("sending this token: ", userData.token);
+			API.getCart(userData.token).then((res) => {
+				console.log("CART RECEIVED");
+				console.log("RESPONSE: ", res);
+				setCartId(res.cartId);
+				setCartProducts(res.products);
+			});
 		}
-/* 		else if(storagedData?.user?.cart) setCartProducts(storagedData.user.cart)
+		/* 		else if(storagedData?.user?.cart) setCartProducts(storagedData.user.cart)
 		if(storagedData?.user?.cartId) setCartId(storagedData.user.cartId) */
-		setLocalStorage({ ...userData, cart: cartProducts, cartId })
+	}, [userData, setCartProducts, update]);
 
-	}, [ userData, setCartProducts, update ])
-
-
-
+	useEffect(() => {
+		setLocalStorage({ ...userData, cart: cartProducts, cartId });
+	}, [cartProducts]);
 
 	function updateCartProducts(newCartProductsArray, product) {
-		console.log("GLOBAL updateCartProducts")
-		console.log("newCartProductsArray: ", newCartProductsArray)
-		console.log("cartProducts: ", cartProducts)
-		console.log("product: ", product)
-		console.log("cartId: ", cartId)
+		console.log("GLOBAL updateCartProducts");
+		console.log("newCartProductsArray: ", newCartProductsArray);
+		console.log("cartProducts: ", cartProducts);
+		console.log("product: ", product);
+		console.log("cartId: ", cartId);
 
-		console.log("newCartArray.length: ", newCartProductsArray.length)
-		console.log("savedCartLength: ", cartProducts.length)
+		console.log("newCartArray.length: ", newCartProductsArray.length);
+		console.log("savedCartLength: ", cartProducts.length);
 
-		if(newCartProductsArray.length > cartProducts.length && userData.token) {
-			console.log("ADICIONANDO")
+		setCartProducts(newCartProductsArray);
+
+		if (newCartProductsArray.length > cartProducts.length && userData.token) {
+			console.log("ADICIONANDO");
 			API.addToCart(userData.token, {
 				cartId,
 				productId: product.id,
-				amount: product.quantity
+				amount: product.quantity,
 			})
-			.then(res => { setCartProducts(res.products) })
-			.catch(e => {
-				console.log(e)
-				alert("ERRO update cart products")
-			})
-		}
-
-		else if(userData.token) {
-			console.log("REMOVENDO")
-			console.log("removing product: ", product)
-			console.log("sendingToken: ", userData.token)
+				.then((res) => {
+					setCartProducts(res.products);
+				})
+				.catch((e) => {
+					console.log(e);
+					alert("ERRO update cart products");
+				});
+		} else if (userData.token) {
+			console.log("REMOVENDO");
+			console.log("removing product: ", product);
+			console.log("sendingToken: ", userData.token);
 			API.removeFromCart(userData.token, {
 				cartId,
-				productId: product.id
+				productId: product.id,
 			})
-			.then(res => { setCartProducts(res.products) })
-			.catch(e => {
-				console.log(e)
-				alert("ERRO updateCartProducts")
-			})
+				.then((res) => {
+					setCartProducts(res.products);
+				})
+				.catch((e) => {
+					console.log(e);
+					alert("ERRO updateCartProducts");
+				});
 		}
 
-
-
-/* 		setUserData({
+		/* 		setUserData({
 			...userData,
 			user: { ...userData.user, cart: cartProductsArray },
 		});
