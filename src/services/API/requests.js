382ab440e9721b494bc066576b2053db5266dd8c
yasmimc <yasmimcavalcanti.r@gmail.com>
2021-11-15 17:51:55 -0300

    validate local storaged token

--- src/services/API/requests.js
+++ src/services/API/requests.js
@@ -3,12 +3,13 @@ import axiosBase from "./axiosBase";
 const API = {
 	signUp,
 	signIn,
+	validateToken,
 	logOut,
 	getProducts,
 	updateCart,
 	getCart,
 	addToCart,
-	removeFromCart
+	removeFromCart,
 };
 
 function createBearerAuthorization(token) {
@@ -36,6 +37,10 @@ function signIn({ cpf, email, password }) {
 	});
 }
 
+function validateToken(token) {
+	return axiosBase.get("/sessions", createBearerAuthorization(token));
+}
+
 function logOut({ token }) {
 	return axiosBase.delete("/sessions", createBearerAuthorization(token));
 }
@@ -46,46 +51,60 @@ function getProducts() {
 
 // returns the cartId and all its products, if there is no products, return an empty array
 async function getCart(token) {
-    const response = await axiosBase.get("/cart", createBearerAuthorization(token))
-	const productsArray = response.data.products.map(product => ({ ...product, quantity: product.amount }))
+	const response = await axiosBase.get(
+		"/cart",
+		createBearerAuthorization(token)
+	);
+	const productsArray = response.data.products.map((product) => ({
+		...product,
+		quantity: product.amount,
+	}));
 
 	return {
 		cartId: response.data.cartId,
-		products: productsArray
-	}
+		products: productsArray,
+	};
 }
 
 // body = { cartId, productId, amount: quantity }
 async function addToCart(token, body) {
-	const response = await axiosBase.put("/cart", body, createBearerAuthorization(token))
-	const productsArray = response.data.products.map(product => ({ ...product, quantity: product.amount }))
+	const response = await axiosBase.put(
+		"/cart",
+		body,
+		createBearerAuthorization(token)
+	);
+	const productsArray = response.data.products.map((product) => ({
+		...product,
+		quantity: product.amount,
+	}));
 	return {
 		cartId: response.data.cartId,
-		products: productsArray
-	}
+		products: productsArray,
+	};
 }
 
 async function removeFromCart(token, body) {
-	console.log("SERVICE")
-	console.log("body: ", body)
-	console.log("token: ", token)
+	console.log("SERVICE");
+	console.log("body: ", body);
+	console.log("token: ", token);
 	const response = await axiosBase.delete("/cart", {
 		headers: {
-			Authorization: `Bearer ${token}`
+			Authorization: `Bearer ${token}`,
 		},
 		data: {
-			...body
-		}
-	})
-	const productsArray = response.data.products.map(product => ({ ...product, quantity: product.amount }))
+			...body,
+		},
+	});
+	const productsArray = response.data.products.map((product) => ({
+		...product,
+		quantity: product.amount,
+	}));
 	return {
 		cartId: response.data.cartId,
-		products: productsArray
-	}
+		products: productsArray,
+	};
 }
 
-
-
-function updateCart() { }
+function updateCart() {}
 
 export default API;
